53c04126399755664a3b8923d3210bc3896ad8cd
shawn sandy <shawnsandy04@gmail.com>
2016-10-27 12:46:05 -0400

    First commit

---
+++ src/PkgStartServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ShawnSandy\PkgStart;
+
+use Illuminate\Support\ServiceProvider;
+
+class PkgStartServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
