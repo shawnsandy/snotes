3e2e45dda9a35314d71ff3d73c851f8097ec7686
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-10-31 06:51:27 -0400

    updates dir paths in publish method

--- src/Provider.php
+++ src/Provider.php
@@ -22,14 +22,26 @@ class Provider extends ServiceProvider
             include __DIR__ . '/App/routes.php';
         }
 
+        /**
+         * Package views
+         */
+        $this->loadViewsFrom(__DIR__ . '/resources/views', '__PACKAGE_NAME__');
+        $this->publishes(
+            [
+                __DIR__ . '/resources/views' => resource_path('views/vendor/__PACKAGE_NAME__'),
+            ], 'toastr-alerts'
+        );
 
+        /**
+         * Package config
+         */
         $this->publishes(
             [__DIR__ . '/config/pagekit.php' => config_path('pagekit.php')],
             'pagekit-config'
         );
 
         if (!$this->app->runningInConsole()) :
-            include_once __DIR__ . 'App/Helpers/helper.php';
+            include_once __DIR__ . '/App/Helpers/helper.php';
         endif;
 
 
@@ -47,8 +59,8 @@ class Provider extends ServiceProvider
             __DIR__ . 'App/config/config.php', '__YOUR_KEY_NAME__'
         );
         $this->app->bind(
-            '__YOUR_FACADENAME__', function () {
-                return new YOUR_CLASSNAME();
+            '__YOUR_FACADE_NAME__', function () {
+                return new YOUR_CLASS_NAME();
             }
         );
       *** remove this line to uncomment ***/
