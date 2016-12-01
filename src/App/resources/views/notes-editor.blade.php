7bd3d132fb3e7c3c161f65594389d60991d7bc69
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-12-01 06:51:05 -0500

    Editor views

--- src/App/resources/views/notes-editor.blade.php
+++ src/App/resources/views/notes-editor.blade.php
@@ -2,6 +2,22 @@
 //php here
 @endphp
 <div class="adv-notes">
-    <textarea name="{{ $notes['editor_name'] or "adv-notes" }}" id="adv-notes" class="notes">
+    <div class="notes-toolbar">
+        <div class="col notes-logo-text text-uppercase">Notes </div>
+        <input type="title" class="col-fluid form-control" placeholder="Whats the title of your note?">
+    </div>
+    <textarea name="{{ $notes['selector'] or "adv-notes" }}" id="adv-notes" class="notes">
     </textarea>
+    <div class="notes-toolbar">
+        <button class="col">Image Library</button>
+        <button class="col">Upsplash Photos</button>
+        <div class="spacer col-fluid"></div>
+        <button class="col">Save Note</button>
+    </div>
 </div>
+@push('styles')
+<link rel="stylesheet" href="{{ asset('assets/notes/css/notes.css') }}">
+@endpush
+@push('scripts')
+
+@endpush
--- src/App/resources/views/partials/notes-adv-js.blade.php
+++ src/App/resources/views/partials/notes-adv-js.blade.php
@@ -3,28 +3,14 @@
 @endphp
 <script>
     $(document).ready(function () {
-        var el = "{{ $notes['selector'] or "#summernote" }}";
+        var el = "{{ $notes['selector'] or ".adv-notes #adv-notes" }}";
         $(el).summernote({
+            placeholder: "{{ $notes['placeholder'] or "Create a new note here..." }}",
             minHeight: "{{ $notes['min_height'] or '300px' }}",
             focus: {{ $notes['focus'] or 'true' }},
             airMode: {{ $notes['airmode'] or 'false' }},
-            focus: {{ $notes['focus'] or 'true' }},
-            placeholder: "{{ $notes['placeholder'] or "Create a new note here..." }}",
-            callbacks: {
-                onImageUpload: function(files) {
-                    console.log(files);
-                    // upload image to server and create imgNode...
-//                    $summernote.summernote('insertNode', imgNode);
-                }
-            }
+            focus: {{ $notes['focus'] or 'false' }}
+
         });
-        $(el).on('summernote.image.upload', function(we, files) {
-            // upload image to server and create imgNode...
-            console.log('super notes');
-//            $summernote.summernote('insertNode', imgNode);
-        });
-
-
-
     });
 </script>
