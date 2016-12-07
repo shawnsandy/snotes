b84686e5581c21289446f0f9035735860f1b9f2f
shawn sandy <shawnsandy04@gmil.com>
2016-12-06 21:09:42 -0500

    Controller setup

---
+++ src/App/Controllers/NotesController.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: studio-book
+ * Date: 12/6/16
+ * Time: 6:36 PM
+ */
+
+namespace ShawnSandy\Summernote\App\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class NotesController extends Controller
+{
+
+    public function index()
+    {
+        return view('notes::index');
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function create()
+    {
+        $images = [];
+        return view('notes::create', compact('images'));
+    }
+
+    public function  store(Request $request){
+
+    }
+
+
+    public function edit($id){
+
+    }
+
+    public function update(Request $request, $id)  {
+
+    }
+
+    public function destroy($id){
+
+    }
+
+}
