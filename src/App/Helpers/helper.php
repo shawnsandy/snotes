75fc2e20dfe0e8f89cd409b660b0cf7ecdfe9a38
shawn sandy <shawnsandy04@gmil.com>
2016-12-06 21:13:11 -0500

    Added font-awesome script function

--- src/App/Helpers/helper.php
+++ src/App/Helpers/helper.php
@@ -1,18 +1,40 @@
 <?php
 
-
-    function notesJs(){
-        return "<script src=\"".public_path("assets/summernote/summernote.min.js")."\"></script>";
+if (!function_exists('notesJS')):
+    function notesJs()
+    {
+        return "<script src=\"" . public_path("assets/summernote/summernote.min.js") . "\"></script>";
     }
+endif;
 
-    function notesStyle(){
-        return "<link href=\"".public_path("assets/summernote/summernote.css")."\" rel=\"stylesheet\">";
-    }
 
-    function notesJsCdn(){
-        return "<script src=\"http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.js\"></script>";
-    }
+if (!function_exists('notesFontAwesomeCdn')):
+    /*https://www.bootstrapcdn.com/fontawesome/*/
+    function notesFontAwesomeCdn()
+    {
 
-    function notesStyleCdn(){
-        return "<link href=\"http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.css\" rel=\"stylesheet\">";
+        return "<link href=\"https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css\" rel=\"stylesheet\">";
     }
+endif;
+
+if (!function_exists('notesStyle')):
+    function notesStyle()
+    {
+        return "<link href=\"" . public_path("assets/summernote/summernote.css") . "\" rel=\"stylesheet\">";
+    }
+endif;
+
+if(!function_exists('notesJsCdn')):
+function notesJsCdn()
+{
+    return "<script src=\"http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.js\"></script>";
+}
+endif;
+
+
+if(!function_exists('notesStyleCdn')):
+function notesStyleCdn()
+{
+    return "<link href=\"http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.css\" rel=\"stylesheet\">";
+}
+endif;
