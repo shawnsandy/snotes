79a3095c4819ddf18a07c093f179d8444619224e
shawn sandy <shawnsandy04@gmil.com>
2016-12-07 21:28:33 -0500

    adds methods from unsplash

--- src/App/Notes/Notes.php
+++ src/App/Notes/Notes.php
@@ -10,14 +10,16 @@ namespace ShawnSandy\Summernote\App\Notes;
 
 
 use Illuminate\Support\Facades\Storage;
-use Crew\Unsplash as Unsplash;
 
 class Notes
 {
 
     protected $files;
+    protected $unsplash ;
 
     public function __construct() {
+        $this->unsplash = new Unsplash();
+
     }
 
     public function getImages(){
@@ -25,33 +27,15 @@ class Notes
         return Storage::disk('images')->files('notes');
     }
 
-    public function getUnsplashPhotos(){
 
-        Unsplash\HttpClient::init([
-            'applicationId' => 'ff1c6c90eec52a82c30dbadbcb0acdc04e403d8a749e16a03909f6978b31d8a9',
-            'secret' => 'e89a4910547363b6693a49f17611242b51c34a9ac5bebaa9e95c1db43f62140a',
-        ]);
-        $scopes = ['public', 'write_user'];
-        Unsplash\HttpClient::$connection->getConnectionUrl($scopes);
-        $response = (new Unsplash\HttpClient())->send("get", ['/collections/450657/photos']);
-        $body = collect(json_decode($response->getBody(), true));
+    public function getUnsplashCollection(){
+        $collection = $this->unsplash->unsplashCollection();
+        return  $collection;
+    }
 
-        $images = $body->map(function ($item, $key) {
-            return [
-                "username" => $item['user']['first_name'].' '.$item['user']['last_name'],
-                "id" => $item['user']['id'],
-                "user_profile" => $item['user']['links']['html'],
-                "avatar" => $item['user']['profile_image']['small'],
-                "photo_raw" => $item['urls']['raw'],
-                "photo_full" => $item['urls']['full'],
-                "photo_regular" => $item['urls']['regular'],
-                "photo_small" => $item['urls']['small'],
-                "photo_thumb" => $item['urls']['thumb']
-            ];
-        });
-
+    public function getUnsplashCollectionPhotos(){
+        $images = $this->unsplash->unsplashCollectionPhotos();
         return $images;
-
     }
 
 }
