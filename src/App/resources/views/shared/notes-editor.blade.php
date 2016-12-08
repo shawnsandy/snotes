30cf5c26a1ef49bbca588a1e13f8f251cc2b7620
shawn sandy <shawnsandy04@gmil.com>
2016-12-07 19:01:43 -0500

    Updates button links / type and actions

--- src/App/resources/views/shared/notes-editor.blade.php
+++ src/App/resources/views/shared/notes-editor.blade.php
@@ -5,18 +5,20 @@
     <div class="notes-toolbar">
         <div class="col notes-logo-text text-uppercase">Notes <i class="fa fa-pencil" aria-hidden="true"></i></div>
         <input name="snotes_title" class="col-fluid form-control" placeholder="Hi, whats the title of your note? (Enter here)">
-        <button class="col" data-toggle="modal" data-target="#sm-image-library"><i class="fa fa-pencil" aria-hidden="true"></i> Images Upload</button>
+        <button class="col" type="button" data-toggle="modal" data-target="#sm-image-library"><i class="fa fa-file-image-o" aria-hidden="true"></i> Images</button>
         @if(function_exists('unsplashSearch'))
-            <button class="col">Unsplash Photos</button>
+            <button class="col"><i class="fa fa-camera" aria-hidden="true"></i> Unsplash Photos</button>
         @endif
     </div>
     <textarea name="{{ $notes['selector'] or "snotes_description" }}" id="adv-notes" class="notes"></textarea>
     <div class="notes-toolbar">
         <div class="spacer col-fluid"></div>
-        <button type="submit" class="col"><i class="fa fa-check" aria-hidden="true"></i> Save Note</button>
+        <button type="submit" class="col"><i class="fa fa-file-text" aria-hidden="true"></i> Save Note</button>
     </div>
+
+    {{-- modal --}}
     <div id="sm-image-library" class="modal fade" tabindex="-1" role="dialog">
-        <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-dialog modal-md" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
