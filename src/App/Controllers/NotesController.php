250acf9fcc2223ee4aa0ba9a1e18432e120b132a
shawnsandy04 <shawnsandy04@gmail.com>
2016-12-27 23:19:42 -0500

    Update store method to save/create notes

--- src/App/Controllers/NotesController.php
+++ src/App/Controllers/NotesController.php
@@ -9,8 +9,10 @@
 namespace ShawnSandy\Summernote\App\Controllers;
 
 
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Auth;
 use ShawnSandy\Summernote\App\Models\Snotes;
 use ShawnSandy\Summernote\App\Notes\StoreNotesRequest;
 
@@ -35,7 +37,11 @@ class NotesController extends Controller
     }
 
     public function  store(StoreNotesRequest $request){
-        return $request->all();
+        $id = Auth::id();
+        $user = User::find($id);
+//        $notes = $user->notes()->create($request->all());
+        $notes = $request->user()->snotes()->create($request->all());
+        return $notes;
     }
 
 
