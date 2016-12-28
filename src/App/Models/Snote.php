72ebd647c160827e9269ae43db6f334de2ceb634
shawnsandy04 <shawnsandy04@gmail.com>
2016-12-27 23:25:56 -0500

    Update Class

---
+++ src/App/Models/Snote.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: studio-book
+ * Date: 12/8/16
+ * Time: 5:42 AM
+ */
+
+namespace ShawnSandy\Summernote\App\Models;
+
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Snote extends Model
+{
+    protected $fillable = ['notes_title', 'notes_content'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+}
