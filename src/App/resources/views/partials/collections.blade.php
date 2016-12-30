9e3d00c04124f8ac9fdafd11f17ad653584aa958
shawnsandy04 <shawnsandy04@gmail.com>
2016-12-30 06:54:36 -0500

    WIP - layout tweaks

--- src/App/resources/views/partials/collections.blade.php
+++ src/App/resources/views/partials/collections.blade.php
@@ -1,10 +1,10 @@
-<div class="col-md-6">
+<div class="col-md-12">
     <div class="a-note">
         <div class="h1 text-capitalize ">
             <a href="/snotes/{{ $notes->id }}" class="">
-                {{ $notes->notes_title }}
+                <i class="fa fa-chevron-circle-right" aria-hidden="true"></i>  {{ $notes->notes_title }}
             </a>
-            <p class="lead text-capitalize">{{ $notes->user->name }}</p>
+
         </div>
     </div>
 
