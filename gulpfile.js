fbc09c380ea5e7d1ad018120b2f5f6ca521ec581
shawnsandy04 <shawnsandy04@gmail.com>
2016-12-29 23:00:46 -0500

    Add sass/watch:sass task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,16 @@
  * Created by shawnsandy on 11/6/16.
  */
 
-gulp = require('gulp');
+var gulp = require('gulp');
+var q = require('q');
+var path = require('path');
+var fs = require('fs');
+var _ = require('underscore');
+var imagemin = require('gulp-imagemin');
+var sass = require('gulp-sass');
+var notify = require('gulp-notify');
+var changed = require('gulp-changed');
+var toast = require('node-notifier');
 
 gulp.task('copy:js', function () {
     gulp.src(['/node'])
@@ -18,13 +27,12 @@ gulp.task('build', function () {
 });
 
 gulp.task('sass', function () {
-    return gulp.src('./src/resources/assets/**/*.scss', {'base': './src/resources/assets/'})
-        .pipe(changed('./src/resources/assets/**/*.scss'))
-        .pipe(sass().on('error', sass.logError))
+    return gulp.src('./src/resources/assets/css/**/*.scss', {'base': './src/resources/assets/'})
+        .pipe(sass().on('error', notify.onError('Error processing')))
         .pipe(gulp.dest('./src/resources/assets'))
-        .pipe(notify({
-            title: "Pages Notification",
-            message: "SCSS files compiled, enjoy",
-            onLast: true
-        }))
+
+});
+
+gulp.task('watch:sass', function () {
+    gulp.watch('./src/resources/assets/css/**/*.scss', ['sass']);
 });
