a4cc23bbc3d2a9c2e271e86ac5dc9a6b27aef335
shawnsandy04 <shawnsandy04@gmail.com>
2017-01-01 08:46:32 -0500

    Set the element width inline

--- src/App/resources/views/partials/collections.blade.php
+++ src/App/resources/views/partials/collections.blade.php
@@ -1,10 +1,17 @@
 <div class="col-md-12">
     <div class="a-note">
         <div class="layout">
-            <div class="layout-center">
+            <div class="layout-center" style="width: 100px">
                 <div class="text-capitalize">
-                    <a href="/snotes/{{ $notes->id }}" class="note-title">
-                        <i class="fa fa-chevron-circle-right" aria-hidden="true"></i> {{ $notes->notes_title }}
+                    <a href="/snotes/{{ $notes->id }}}" class="note-title">
+                        <i class="fa fa-chevron-circle-right" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="layout-center">
+                <div class="text-capitalize text-left">
+                    <a href="/snotes/{{ $notes->id }}}" class="note-title">
+                        {{ $notes->notes_title }}
                     </a>
                 </div>
             </div>
