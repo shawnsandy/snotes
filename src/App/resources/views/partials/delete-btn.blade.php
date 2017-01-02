1f802dd26bbd836ef8e38c20e70db6b9b97c057d
shawnsandy04 <shawnsandy04@gmail.com>
2017-01-02 15:08:45 -0500

    #2 Confimation on note that is trashed

--- src/App/resources/views/partials/delete-btn.blade.php
+++ src/App/resources/views/partials/delete-btn.blade.php
@@ -7,7 +7,22 @@
      */
 ?>
 
-<form action="/photos/{{ $id }}">
+<form action="/photos/{{ $id }}" id="delete-action">
     {{ method_field('DELETE') }}
-    <button type="submit" class="btn btn-default"><i class="fa fa-trash" aria-hidden="true"></i>Move to trash</button>
+    <button  class="btn btn-default delete trash" type="button"><i class="fa fa-trash" aria-hidden="true"></i> Move to trash</button>
+    <button type="submit" class="btn btn-default confirm-trash"><i class="fa fa-times" aria-hidden="true"></i> Continue to trash</button>
+    <button  class="btn btn-default delete confirm-trash trash" type="button"><i class="fa fa-times" aria-hidden="true"></i> Cancel</button>
 </form>
+@push('scripts')
+<script>
+    $(document).ready(function(){
+        var trash = $(this).find('.trash');
+        var buttons = $(this).find('.btn');
+        $(trash).click(function(e){
+            e.preventDefault();
+             buttons.slideToggle('slow');
+            console.log('trash clicked')
+        })
+    });
+</script>
+@endpush
