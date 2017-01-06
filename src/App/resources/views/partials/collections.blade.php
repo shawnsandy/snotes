1b571dcc0c63f5941e272c9edcb64a138899db7d
shawnsandy04 <shawnsandy04@gmail.com>
2017-01-06 07:04:56 -0500

    move link button to left

--- src/App/resources/views/partials/collections.blade.php
+++ src/App/resources/views/partials/collections.blade.php
@@ -1,13 +1,7 @@
 <div class="col-md-12">
     <div class="a-note">
         <div class="layout">
-            <div class="layout-center" style="width: 100px">
-                <div class="text-capitalize">
-                    <a href="/snotes/{{ $notes->id }}}" class="note-title">
-                        <i class="fa fa-chevron-circle-right" aria-hidden="true"></i>
-                    </a>
-                </div>
-            </div>
+
             <div class="layout-center">
                 <div class="text-capitalize text-left">
                     <a href="/snotes/{{ $notes->id }}}" class="note-title">
@@ -15,6 +9,15 @@
                     </a>
                 </div>
             </div>
+
+            <div class="layout-center" style="width: 100px">
+                <div class="text-capitalize text-right">
+                    <a href="/snotes/{{ $notes->id }}}" class="note-title direction">
+                        <i class="fa fa-angle-double-right" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </div>
+            
         </div>
     </div>
 </div>
