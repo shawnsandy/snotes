d2fc24272cf1d3584feee0fcab10937a9d328aba
shawnsandy04 <shawnsandy04@gmail.com>
2017-01-10 06:56:12 -0500

    Slide -- on the fly image manipulation

--- src/App/routes.php
+++ src/App/routes.php
@@ -7,7 +7,10 @@
  */
 
 Route::group(['middleware' => ['web']], function () {
+
     //  your routes goes here
     Route::resource('/snotes/new', 'ShawnSandy\Summernote\App\Controllers\NotesController@create');
     Route::resource('/snotes', 'ShawnSandy\Summernote\App\Controllers\NotesController');
+    Route::get('/snotes/img/{path}', 'ShawnSandy\Summernote\App\Controllers\ImagesController')->where('path', '.*');
+
 });
