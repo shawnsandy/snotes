2c7cc085eb25268dd31679cc4dff81bd5ba999cf
shawnsandy04 <shawnsandy04@gmail.com>
2017-01-10 19:12:25 -0500

    renamed routes - snotes to notes

--- src/App/routes.php
+++ src/App/routes.php
@@ -9,8 +9,8 @@
 Route::group(['middleware' => ['web']], function () {
 
     //  your routes goes here
-    Route::resource('/snotes/new', 'ShawnSandy\Summernote\App\Controllers\NotesController@create');
-    Route::resource('/snotes', 'ShawnSandy\Summernote\App\Controllers\NotesController');
-    Route::get('/snotes/img/{path}', 'ShawnSandy\Summernote\App\Controllers\ImagesController')->where('path', '.*');
+    Route::resource('/notes/new', 'ShawnSandy\Summernote\App\Controllers\NotesController@create');
+    Route::resource('/notes', 'ShawnSandy\Summernote\App\Controllers\NotesController');
+    Route::get('/notes/img/{path}', 'ShawnSandy\Summernote\App\Controllers\ImagesController')->where('path', '.*');
 
 });
