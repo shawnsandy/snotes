aa8cb0e569899afa49f50ed53771e794a49dfe3c
shawnsandy04 <shawnsandy04@gmail.com>
2017-01-11 21:52:18 -0500

    Replace the double arrow on collection with circle

--- src/App/resources/views/partials/collections.blade.php
+++ src/App/resources/views/partials/collections.blade.php
@@ -2,18 +2,18 @@
     <div class="a-note">
         <div class="layout">
 
-            <div class="layout-center">
-                <div class="text-capitalize text-left">
+            <div class="layout-center" style="width: 100px">
+                <div>
                     <a href="/notes/{{$notes->id}}" class="note-title">
-                        {{ $notes->notes_title }}
+                        <i class="fa fa-circle-o"></i>
                     </a>
+
                 </div>
             </div>
-
-            <div class="layout-center" style="width: 100px">
-                <div class="text-capitalize text-right">
-                    <a href="/notes/{{$notes->id}}" class="note-title direction">
-                        <i class="fa fa-angle-double-right" aria-hidden="true"></i>
+            <div class="layout-center">
+                <div class="text-capitalize text-left">
+                    <a href="" class="note-title">
+                      {{ $notes->notes_title }}
                     </a>
                 </div>
             </div>
