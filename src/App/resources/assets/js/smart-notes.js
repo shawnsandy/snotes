6e6586534fc3e5d234197166e16406ff009eb3cf
shawnsandy04 <shawnsandy04@gmail.com>
2017-01-11 22:39:09 -0500

    Code review fixes - replace single quotes

--- src/App/resources/assets/js/smart-notes.js
+++ src/App/resources/assets/js/smart-notes.js
@@ -4,11 +4,11 @@
 
 $(document).ready(function () {
 
-    $('.smart-editor').each(function () {
+    $(".smart-editor").each(function () {
 
-        var editor = $(this).data('notesEditor');
-        var imgBtn = $(this).data('insertImg');
-        var placeholder = $(this).data('notesPlaceholder') ? $(this).data('notesPlaceholder') : 'Now lets add some details here...';
+        var editor = $(this).data("notesEditor");
+        var imgBtn = $(this).data("insertImg");
+        var placeholder = $(this).data("notesPlaceholder") ? $(this).data("notesPlaceholder") : "Now lets add some details here...";
 
         /* instantiate the editor */
         $(editor).summernote({
@@ -17,21 +17,21 @@ $(document).ready(function () {
             airMode: true
         });
 
-        $('.add-image').each(function () {
-            console.log('found');
+        $(".add-image").each(function () {
+            console.log("found");
             $(this).click(function (e) {
                 e.preventDefault();
                 var src = $(this).data('src');
                 console.log(src);
-                $(editor).summernote('insertImage', src);
-                $('#img-modal').modal('hide')
+                $(editor).summernote("insertImage", src);
+                $('#img-modal').modal("hide");
             })
         });
 
     });
 
-    var trash = $(this).find('.trash');
-    var buttons = $(this).find('.btn');
+    var trash = $(this).find(".trash");
+    var buttons = $(this).find(".btn");
     $(trash).click(function (e) {
         e.preventDefault();
         buttons.toggle();
