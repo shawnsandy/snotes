8558e3beb89cb351c6244297f6ff58083a0abcde
shawnsandy04 <shawnsandy04@gmail.com>
2017-01-12 22:38:48 -0500

    Add another button for thumbnails
    - btn sizes 250, 500, 800, 1200

--- src/App/resources/views/partials/images.blade.php
+++ src/App/resources/views/partials/images.blade.php
@@ -2,12 +2,15 @@
     <div class="col-md-7"><img src="{{ url("/notes/img/$img?w=250") }}" alt="images"></div>
     <div class="col-md-5 text-center">
         <button data-src="{{url("/notes/img/$img?w=250")}}" type="button" class="btn btn-block add-image">
+            Thumbnail
+        </button>
+        <button data-src="{{url("/notes/img/$img?w=480")}}" type="button" class="btn btn-block add-image">
             Small Photo
         </button>
-        <button data-src="{{url("/notes/img/$img?w=500")}}" type="button" class="btn btn-block add-image">
+        <button data-src="{{url("/notes/img/$img?w=800")}}" type="button" class="btn btn-block add-image">
             Medium Photo
         </button>
-        <button data-src="{{url("/notes/img/$img?w=800")}}" type="button" class="btn btn-block add-image">
+        <button data-src="{{url("/notes/img/$img?w=1200")}}" type="button" class="btn btn-block add-image">
             Large Photo
         </button>
     </div>
