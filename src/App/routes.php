d3df25b4615c8644b57fd0cf9a62c81a5f001bb6
shawnsandy04 <shawnsandy04@gmail.com>
2017-01-17 21:32:10 -0500

    Added auth / modified routes to faclitate

--- src/App/routes.php
+++ src/App/routes.php
@@ -8,12 +8,17 @@
 
 Route::group(['middleware' => ['web']], function () {
 
-    //  your routes goes here
-    Route::get('/snotes/new', 'ShawnSandy\Summernote\App\Controllers\NotesController@create');
-    Route::get('/snotes/trash/{id}', 'ShawnSandy\Summernote\App\Controllers\NotesController@destroy');
-    Route::resource('/snotes', 'ShawnSandy\Summernote\App\Controllers\NotesController');
+    Route::get('/snotes/index', 'ShawnSandy\Summernote\App\Controllers\NotesController@index');
+    Route::get('/snotes/{id}', 'ShawnSandy\Summernote\App\Controllers\NotesController@show');
     Route::get('/snotes/img/{path}', 'ShawnSandy\Summernote\App\Controllers\ImagesController')->where('path', '.*');
 
+    Route::group(['middleware' => 'auth'], function(){
+        //  your routes goes here
+        Route::get('/snotes/new', 'ShawnSandy\Summernote\App\Controllers\NotesController@create');
+        Route::get('/snotes/trash/{id}', 'ShawnSandy\Summernote\App\Controllers\NotesController@destroy');
+        Route::resource('/snotes', 'ShawnSandy\Summernote\App\Controllers\NotesController');
+
+    } );
 
 });
 
