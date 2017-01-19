b88bf92a136b0c339c419ff9d09f9ff8aea2721b
shawnsandy04 <shawnsandy04@gmail.com>
2017-01-19 07:16:53 -0500

    Fixes issues with routes order

--- src/App/routes.php
+++ src/App/routes.php
@@ -1,24 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: shawnsandy
- * Date: 10/27/16
- * Time: 12:58 PM
- */
+    /**
+     * Created by PhpStorm.
+     * User: shawnsandy
+     * Date: 10/27/16
+     * Time: 12:58 PM
+     */
 
-Route::group(['middleware' => ['web']], function () {
+    Route::group(['middleware' => ['web']], function () {
 
-    Route::get('/snotes/index', 'ShawnSandy\Summernote\App\Controllers\NotesController@index');
-    Route::get('/snotes/{id}', 'ShawnSandy\Summernote\App\Controllers\NotesController@show');
-    Route::get('/snotes/img/{path}', 'ShawnSandy\Summernote\App\Controllers\ImagesController')->where('path', '.*');
 
-    Route::group(['middleware' => 'auth'], function(){
-        //  your routes goes here
-        Route::get('/snotes/new', 'ShawnSandy\Summernote\App\Controllers\NotesController@create');
-        Route::get('/snotes/trash/{id}', 'ShawnSandy\Summernote\App\Controllers\NotesController@destroy');
-        Route::resource('/snotes', 'ShawnSandy\Summernote\App\Controllers\NotesController');
+        Route::group(['middleware' => 'auth'], function () {
+            //  your routes goes here
+            Route::get('/snotes/new', 'ShawnSandy\Summernote\App\Controllers\NotesController@create');
+            Route::get('/snotes/trash/{id}', 'ShawnSandy\Summernote\App\Controllers\NotesController@destroy');
+            Route::resource('/snotes', 'ShawnSandy\Summernote\App\Controllers\NotesController');
 
-    } );
+        });
+        Route::get('/snotes/index', 'ShawnSandy\Summernote\App\Controllers\NotesController@index');
+        Route::get('/snotes/img/{path}', 'ShawnSandy\Summernote\App\Controllers\ImagesController')->where('path', '.*');
+        Route::get('/snotes/{id}', 'ShawnSandy\Summernote\App\Controllers\NotesController@show');
 
-});
+    });
 
