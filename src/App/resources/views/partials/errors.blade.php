9f64e774ea50117ea8da4af3c8b19a6b907a337e
shawnsandy04 <shawnsandy04@gmail.com>
2017-02-24 06:30:35 -0500

    test update

--- src/App/resources/views/partials/errors.blade.php
+++ src/App/resources/views/partials/errors.blade.php
@@ -6,6 +6,14 @@
     <p class="alert alert-success">{{ Session::get('success') }}</p>
 @endif
 
+@if(Session::has('warning'))
+    <p class="alert alert-warning">{{ Session::get('warning') }}</p>
+@endif
+
+@if(Session::has('info'))
+    <p class="alert alert-info">{{ Session::get('info') }}</p>
+@endif
+
 @if($errors->has('notes_title'))
     <p class="alert alert-danger"><i class="fa fa-times-circle" aria-hidden="true"></i> {{ $errors->first('notes_title') }}</p>
 @endif
